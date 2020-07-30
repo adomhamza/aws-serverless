58561a71e5173d4b9c1dcf12ec086c32b134c435
adomhamza <liveghost93@gmail.com>
2020-07-30 11:13:54 +0000

    Update

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -4,6 +4,11 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
+import * as AWS from 'aws-sdk'
+
+import * as uuid from 'uuid'
+import { parseUserId } from '../../auth/utils'
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
